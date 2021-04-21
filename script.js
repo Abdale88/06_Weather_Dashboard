d8972aa0fa0b740da1f8687d629b50d9b7cf2172
Abdale Hassan <cabdale989@gmail.com>
2021-04-21 18:09:55 -0500

    Create a function to fetch data from third party API

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ var searchEl = document.querySelector("#search");
 var inputField = document.querySelector("#input-field");
 var stateName = document.querySelector("#states");
 
+arrayEl = [];
 
+function fetchFunc(){
+    searchEl.addEventListener("click", function(event){
+        event.preventDefault();
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ inputField.value + "&appid=b47242be396209257701a75398843e35&cnt=6")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log("this is data >> ", data);
+    })
+
+    })
+
+}
+
+
+fetchFunc();
