3d9f8b24cd509b29fa29f55723f5b2e37f3c5cc5
Abdale Hassan <cabdale989@gmail.com>
2021-04-24 00:08:40 -0500

    add UV index

--- script.js
+++ script.js
@@ -47,11 +47,26 @@ function currentCity(){
     .then(function(data){
         console.log("this is data ", data);
 
-        containerEl.setAttribute("style", "padding-right: 50%; margin: 10px; border: black 3px solid");
-        cityEl.textContent = data.name  + data.weather[0].icon;
-        tempEl.textContent = "Temp: " + data.main.temp + " °F";
-        humidityEl.textContent = "Humidity: " + data.main.humidity + " %";
-        windEl.textContent = "Wind: " + data.wind.speed + " MPH";
+        var latitude = data.coord.lat;
+        var longitude = data.coord.lon
+
+        console.log('LAT ', latitude);
+        console.log('LOG', longitude);
+        
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ latitude+ "&lon="+ longitude+ "&include=time&appid=b47242be396209257701a75398843e35")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(rawData){
+            console.log("datas >> ", rawData);
+
+            containerEl.setAttribute("style", "padding-right: 50%; margin: 10px; border: black 3px solid");
+            cityEl.textContent = data.name + " " + data.weather[0].icon;
+            tempEl.textContent = "Temp: " + data.main.temp + " °F";
+            humidityEl.textContent = "Humidity: " + data.main.humidity + " %";
+            windEl.textContent = "Wind: " + data.wind.speed + " MPH";
+            uvIndex.textContent = "UV Index: " + rawData.current.uvi;
+        }) 
     })
    
     
@@ -70,35 +85,10 @@ function getValues(){
 }
 
 
-function getData(){
-    
-    stateName.addEventListener("click", function(event){
-        var target = event.target.textContent;
-        console.log("text target <> ", target)
-        for(i = 0; i < savedCities.length; i++){
-            if(target === savedCities[i]){
-                console.log("check this out <> ", savedCities[i]);
-                fetch("https://api.openweathermap.org/data/2.5/weather?q="+ target + "&units=imperial"+"&appid=b47242be396209257701a75398843e35")
-                .then(function(response){
-                 return response.json();
-                })
-                .then(function(data){
-                 console.log("this is  current data ", data);
-
-                 containerEl.setAttribute("style", "padding-right: 50%; margin: 10px;  border: black 3px solid");
-                 cityEl.textContent = data.name;
-                 tempEl.textContent = "Temp: " + data.main.temp + " °F";
-                 windEl.textContent = "Wind: " + data.wind.speed + " MPH";
-                 windEl.textContent = "Wind: " + data.wind.speed + " MPH";
-                  })
-            }
-        }
-        
-     })
-}
 
 
-getData();
+
+
 getValues();
 mainFunction();
 
