474c03c35f417f95bced0039371c03ef6e91f6f5
Abdale Hassan <cabdale989@gmail.com>
2021-04-23 19:36:23 -0500

    get data from the API and delete some css tyle

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ var uvIndex = document.querySelector("uvi");
 var savedCities = [];
 var localStorageEl;
 
+var results;
+
+
 
 
 // fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ cityName.value + "&units=imperial"+"&appid=b47242be396209257701a75398843e35&cnt=6")
@@ -18,8 +21,8 @@ var localStorageEl;
 function mainFunction(){
     searchEl.addEventListener("click", function(event){
         event.preventDefault();
-
         currentCity();
+
     if(savedCities.indexOf(cityName.value) === -1){
         savedCities.push(cityName.value);
         localStorage.setItem("cities", JSON.stringify(savedCities))
@@ -41,9 +44,13 @@ function currentCity(){
         return response.json();
     })
     .then(function(data){
-        console.log("this is data >> ", data);
+        console.log("this is data ", data);
+        cityEl.textContent = data.name + " city name";
     })
+   
+    
 }
+
 
 function getValues(){
     savedCities = JSON.parse(localStorage.getItem("cities")) || [] ;
@@ -53,9 +60,34 @@ function getValues(){
         localStorageEl.textContent = savedCities[i];
         stateName.appendChild(localStorageEl);
         localStorageEl.setAttribute("style", "background: red; margin: 10px;  border-radius: 0.5rem; text-align: center")
-       
     }
 }
 
+
+function getData(){
+    
+    stateName.addEventListener("click", function(event){
+        var target = event.target.textContent;
+        console.log("text target <> ", target)
+        for(i = 0; i < savedCities.length; i++){
+            if(target === savedCities[i]){
+                console.log("check this out <> ", savedCities[i]);
+                fetch("https://api.openweathermap.org/data/2.5/weather?q="+ target + "&units=imperial"+"&appid=b47242be396209257701a75398843e35")
+                .then(function(response){
+                 return response.json();
+                })
+                .then(function(data){
+                 console.log("this is  current data ", data);
+                 cityEl.textContent = data.name;
+                  })
+            }
+        }
+        
+     })
+}
+
+
+getData();
 getValues();
 mainFunction();
+
